03363e4534aefa375ca2a7a72c5582b5f6f5c282
mhassan999 <mehditalks@gmail.com>
2024-05-26 05:47:52 -0400

    lab1c

---
+++ lab1c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#Mohammad Mehdi Hassan - mhassan99 - OPS445NCC
+
+name = 'Isaac'
+
+born = 1952
+date = 2024
+
+age = date - born
+
+print(name + ' is ' + str(age) + ' years old!')
